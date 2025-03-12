6966e3ed1dedcc9376b0ace0ece078fc27d5e1b9
heehong <gmlghd98@gmail.com>
2025-03-12 14:56:03 +0900

    Create Route: /logout

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -5,4 +5,9 @@ const authController = require('../controllers/AuthController');
 // 로그인 라우트 추가
 router.post('/login', authController.login);
 
+//클라이언트에서 jwt삭제되면서 로그아웃수행
+router.post('/logout', (_, res) => {
+    res.json(response('success', '로그아웃 되었습니다.'));
+});
+
 module.exports = router;
